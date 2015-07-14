e6eb9bacf5a22488ff4fd0dad73ee96d5a629b08
csatisfaction <andyleesuk@gmail.com>
2015-07-14 17:06:51 +0100

    swapped scenario for it syntax

--- spec/features/photostream_page_spec.rb
+++ spec/features/photostream_page_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 feature 'Photostream Page' do
   context 'user visits page' do
+  	visit('/')
   	it 'has content "Flickr Photo Stream"' do
-  		visit('/')
     	expect(page).to have_content('Flickr Photo Stream')
+	end
+	it 'has at the first image"' do
+    	expect(page).to have_xpath("//img[contains(@src,'https://farm1.staticflickr.com/303/19069651544_530c002fcb_m.jpg')]")
 	end
   end
 end
