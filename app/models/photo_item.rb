948e7a676ff095eeb98748f045b377b84d983f53
csatisfaction <andyleesuk@gmail.com>
2015-07-15 08:48:07 +0100

    photostream spec added

--- app/models/photo_item.rb
+++ app/models/photo_item.rb
@@ -1,10 +1,13 @@
 class PhotoItem 
 	include ActiveModel::Model
 
-	def self.load_photo_stream
+	def self.load_photo_stream(json_photo_stream)
+		photo_stream = JSON.parse(json_photo_stream)
+		photo_stream_obj 
 
 	end
 
 	def title
+
 	end
 end
---
+++ spec/models/photo_stream_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe PhotoStream, type: :model do
+	let(:photo_stream) { PhotoStream.new }
+
+	it "creates and instance of PhotoStream" do
+	  	expect(photo_stream).to be_an_instance_of(PhotoStream)
+	end
+
+
+  	describe "public class methods" do
+		context "responds to its methods" do
+			it "load_photo_stream" do
+				expect(PhotoStream).to respond_to(:load_photo_stream)
+			end
+		end 
+	end
+	describe "public instance methods" do
+		context "responds to its methods" do
+			it "title" do
+				expect(photo_stream).to respond_to(:title)
+			end
+		end
+		context "responds with the correct data" do
+			it "returns the title" do
+		      feed = %q{{
+				"title": "Uploads from everyone",
+				"link": "https://www.flickr.com/photos/",
+				"description": "",
+				"modified": "2015-07-14T16:05:43Z",
+				"generator": "https://www.flickr.com/",
+				"items": []
+				}}
+
+			 	let(:photo_stream) { PhotoStream.load_photo_stream(feed) }
+		      #body = JSON.parse(feed)
+		      #stream_title = body.map { |m| m["title"] }
+
+		      	expect(photo_stream.title).to match(/Uploads from everyone/)
+		    end
+		end
+	end
+end
