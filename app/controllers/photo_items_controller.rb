de679cb3f81b4b9c351eff0a9ec51796e7356fa8
csatisfaction <andyleesuk@gmail.com>
2015-07-15 19:31:23 +0100

    page handles empty feed

--- app/controllers/photo_items_controller.rb
+++ app/controllers/photo_items_controller.rb
@@ -1,8 +1,11 @@
 class PhotoItemsController < ApplicationController
   def index
  
-  	response_body = PhotoStream.get_photo_stream
-  	@photostream = PhotoStream.load_photo_stream(response_body)
-  	@items = @photostream.items.each.collect { |item| PhotoItem.load_photo_item(item) }
+  	response = HTTParty.get('https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1')
+	if response.code == 200 then
+		photo_stream_data = JSON.parse(response.body)
+  		@photostream = PhotoStream.load_photo_stream(photo_stream_data)
+  		@items = @photostream.items.each.collect { |item| PhotoItem.load_photo_item(item) }
+  	end
   end
 end
